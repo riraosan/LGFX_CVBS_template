13885c9c1c35b72ff46112343901e4e59e2e4772
riaosan <fiatpanda750@icloud.com>
2022-10-28 20:16:48 +0900

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 
 #include <M5GFX.h>
 #include <LGFX_8BIT_CVBS.h>
-
+#include <Connect.h>
 static LGFX_8BIT_CVBS display;
+Connect               wifi;
+
+void showConnectState(String message, int color) {
+  display.fillScreen(color);
+  display.setCursor(0, 0);
+  display.println(message);
+}
 
 void setup() {
   display.init();
-  display.print("Hello World");
-  display.fillScreen(TFT_GREEN);
+
+  showConnectState("Connecting...", TFT_RED);
+  wifi.begin();
+  showConnectState("Connected!", TFT_GREEN);
 }
 
 void loop() {
+  wifi.update();
 }
