c365ee707f2893c1b7f0c862302de9803231b9ea
riraosan <fiatpanda750@icloud.com>
2023-01-18 11:09:40 +0900

    first

--- src/LGFX_8BIT_CVBS.h
+++ src/LGFX_8BIT_CVBS.h
@@ -8,54 +8,49 @@ public:
   lgfx::Panel_CVBS _panel_instance;
 
   LGFX_8BIT_CVBS(void) {
-    {                                       // 表示パネル制御の設定を行います。
-      auto cfg = _panel_instance.config();  // 表示パネル設定用の構造体を取得します。
+    {                                       // Configure display panel control settings
+      auto cfg = _panel_instance.config();  // Gets the structure for display panel settings
 
-      // 出力解像度を設定;
-      cfg.memory_width  = 360;  // 出力解像度 幅
-      cfg.memory_height = 240;  // 出力解像度 高さ
+      // output resolution settings
+      cfg.memory_width  = 360;  // Output Resolution Width
+      cfg.memory_height = 240;  // Output resolution Height
 
-      // 実際に利用する解像度を設定;
-      cfg.panel_width  = 360;       // 実際に使用する幅   (memory_width と同値か小さい値を設定する)
-      cfg.panel_height = 240 - 16;  // 実際に使用する高さ (memory_heightと同値か小さい値を設定する)
+      // the actual resolution to be used settings
+      cfg.panel_width  = 360 - 8;   // Actual width   (Set a value equal to or smaller than memory_width)
+      cfg.panel_height = 240 - 16;  // Actual height  (Set a value equal to or smaller than memory_height)
 
-      // 表示位置オフセット量を設定;
-      cfg.offset_x = 4;   // 表示位置を右にずらす量 (初期値 0)
-      cfg.offset_y = 11;  // 表示位置を下にずらす量 (初期値 0)
+      // the amount of display position offset settings
+      cfg.offset_x = 4;  // Amount to shift the display position to the right (initial value 0)
+      cfg.offset_y = 8;  // Amount to shift the display position down (initial value 0)
 
       _panel_instance.config(cfg);
 
-      // 通常は memory_width と panel_width に同じ値を指定し、 offset_x = 0 で使用します。;
-      // 画面端の表示が画面外に隠れるのを防止したい場合は、 panel_width の値をmemory_widthより小さくし、offset_x で左右の位置調整をします。;
-      // 例えば memory_width より panel_width を 32 小さい値に設定した場合、offset_x に 16 を設定することで左右位置が中央寄せになります。;
-      // 上下方向 (memory_height , panel_height , offset_y ) についても同様に、必要に応じて調整してください。;
+      // Normally, you should specify the same values for memory_width and panel_width and use offset_x = 0
+      // If you want to prevent the edge of the screen from being hidden, set the value of panel_width smaller than memory_width and adjust the left/right position with offset_x.
+      // For example, if you set panel_width to 32 less than memory_width, you can set offset_x to 16 to center the left and right positions.
+      // You should do the same for the vertical direction (memory_height , panel_height , offset_y ) and adjust as needed.
     }
 
     {
       auto cfg = _panel_instance.config_detail();
 
-      // 出力信号の種類を設定;
-      // cfg.signal_type = cfg.signal_type_t::NTSC;
+      // output signal type settings
       cfg.signal_type = cfg.signal_type_t::NTSC_J;
-      // cfg.signal_type = cfg.signal_type_t::PAL;
-      // cfg.signal_type = cfg.signal_type_t::PAL_M;
-      // cfg.signal_type = cfg.signal_type_t::PAL_N;
 
-      // 出力先のGPIO番号を設定;
-      // cfg.pin_dac = 26;  // DACを使用するため、 25 または 26 のみが選択できます;
-      cfg.pin_dac = 25;  // DACを使用するため、 25 または 26 のみが選択できます;
+      // GPIO number of output destination settings
+      cfg.pin_dac = 25;  // For using DAC, you can choose only 25 or 26
 
-      // PSRAMメモリ割当の設定;
-      cfg.use_psram = 0;  // 0=PSRAM不使用 / 1=PSRAMとSRAMを半々使用 / 2=全部PSRAM使用;
+      // PSRAM memory allocation settings
+      cfg.use_psram = 0;  // 0=PSRAM not used / 1=Half PSRAM and half SRAM / 2=All using PSRAM
 
-      // 出力信号の振幅の強さを設定;
-      cfg.output_level = 128;  // 初期値128
-      // ※ GPIOに保護抵抗が付いている等の理由で信号が減衰する場合は数値を上げる。;
-      // ※ M5StackCore2 はGPIOに保護抵抗が付いているため 200 を推奨。;
+      // the amplitude strength of the output signal settings
+      // ※ If the signal is attenuated because the GPIO has a protective resistor or for some other reason, you should increase the value.
+      // ※ Since M5StackCore2 has protection resistors on the GPIOs, we recommend 200.
+      cfg.output_level = 128;  // Initial value 128
 
-      // 彩度信号の振幅の強さを設定;
-      cfg.chroma_level = 128;  // 初期値128
-      // 数値を下げると彩度が下がり、0で白黒になります。数値を上げると彩度が上がります。;
+      // the strength of the amplitude of the saturation signal settings
+      // Decrease the value to decrease saturation, and set to 0 for black and white. Increasing the value increases saturation.
+      cfg.chroma_level = 128;  // Initial value 128
 
       _panel_instance.config_detail(cfg);
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,15 @@
 
 #include <M5GFX.h>
 #include <LGFX_8BIT_CVBS.h>
-#include <Connect.h>
-static LGFX_8BIT_CVBS display;
-Connect               wifi;
 
-void showConnectState(String message, int color) {
-  display.fillScreen(color);
-  display.setCursor(0, 0);
-  display.println(message);
-}
+static LGFX_8BIT_CVBS display;
 
 void setup() {
   display.init();
-
-  showConnectState("Connecting...", TFT_RED);
-  wifi.begin();
-  showConnectState("Connected!", TFT_GREEN);
 }
 
 void loop() {
-  wifi.update();
+  display.setCursor(0, 0);
+  display.println("Hello World");
+  display.display();
 }
